9351102a6c26a25490abf404941032529bd156d4
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-10-30 08:31:46 -0400

    [UPDATE] Recreated Boilerplate File for Customer

--- P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/classes/Customer.py
+++ P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/classes/Customer.py
@@ -1,11 +1,11 @@
 class Customer:
     def __init__(self, name):
         self.name = name
-        
-    def orders(self, new_order=None):
+
+    def access_current_orders(self, new_order=None):
         from classes.order import Order
         pass
-    
-    def pizzas(self, new_pizza=None):
+
+    def access_current_pizzas(self, new_pizza=None):
         from classes.pizza import Pizza
         pass
