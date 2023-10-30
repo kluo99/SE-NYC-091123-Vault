3e4f174b416431e284aaaf83c98e4c7f999254a7
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-10-30 08:32:55 -0400

    [UPDATE] Cleaned Up Testing File for Order

--- P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/testing/order_test.py
+++ P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/testing/order_test.py
@@ -8,7 +8,7 @@ class TestOrders:
     '''Order in order.py'''
 
     def test_has_price(self):
-        '''is initialized with a price'''
+        ''' Tests that an order is initialized with a numerical price. '''
         pizza = Pizza("Cheese")
         customer = Customer('Steve')
         order_1 = Order(customer, pizza, 2)
@@ -18,7 +18,7 @@ class TestOrders:
         assert (order_2.price == 5)
 
     def test_has_a_customer(self):
-        '''order has a customer .'''
+        ''' Tests that an order has a customer.'''
         pizza = Pizza("Cheese")
         customer_1 = Customer('Wayne')
         customer_2 = Customer('Dima')
@@ -29,7 +29,7 @@ class TestOrders:
         assert (order_2.customer == customer_2)
 
     def test_customer_of_type_customer(self):
-        '''customer is of type Customer'''
+        ''' Tests that an order's customer is of type `Customer`. '''
         pizza = Pizza("Mushroom")
         customer = Customer('Steve')
         order_1 = Order(customer, pizza, 2)
@@ -39,7 +39,7 @@ class TestOrders:
         assert (isinstance(order_2.customer, Customer))
 
     def test_has_a_pizza(self):
-        '''Review has a pizza.'''
+        ''' Tests that an order has a pizza. '''
         pizza_1 = Pizza("Cheese")
         pizza_2 = Pizza("Pepperoni")
         customer = Customer('Wayne')
@@ -50,7 +50,7 @@ class TestOrders:
         assert (order_2.pizza == pizza_2)
 
     def test_pizza_of_type_pizza(self):
-        '''pizza is of type Pizza'''
+        ''' Tests that an order's pizza is of type `Pizza`. '''
         pizza = Pizza("Mushroom")
         customer = Customer('Steve')
         order_1 = Order(customer, pizza, 2)
@@ -60,14 +60,14 @@ class TestOrders:
         assert (isinstance(order_2.pizza, Pizza))
 
     def test_get_all_orders(self):
-        '''test Order class all attribute'''
-        Order.all = []
+        ''' Tests that the `Order` class is tracking all current instances in a list attribute. '''
+        Order.catalog = []
         pizza = Pizza("Cheese")
         customer = Customer('Wayne')
         customer_2 = Customer('Dima')
         order_1 = Order(customer, pizza, 2)
         order_2 = Order(customer_2, pizza, 5)
 
-        assert (len(Order.all) == 2)
-        assert (order_1 in Order.all)
-        assert (order_2 in Order.all)
+        assert (len(Order.catalog) == 2)
+        assert (order_1 in Order.catalog)
+        assert (order_2 in Order.catalog)
