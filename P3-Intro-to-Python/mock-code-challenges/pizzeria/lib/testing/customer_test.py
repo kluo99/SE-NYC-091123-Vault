c1f91494c962a786543899750fa7c39dbc9db565
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-10-30 08:32:45 -0400

    [UPDATE] Cleaned Up Testing File for Customer

--- P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/testing/customer_test.py
+++ P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/testing/customer_test.py
@@ -5,21 +5,21 @@ from classes.customer import Customer
 from classes.order import Order
 
 class TestCustomer:
-    '''Customer in customer.py'''
+    ''' Testing class for assessing quality of `customer.Customer` object. '''
 
     def test_has_name(self):
-        '''customer is initialized with name'''
+        ''' Tests that a customer is initialized with a name. '''
         customer = Customer('Steve')
         assert (customer.name == "Steve")
 
     def test_can_change_name(self):
-        '''customer name can be changed'''
+        ''' Tests that a customer's name can be changed after initialization. '''
         customer = Customer('Steve')
         customer.name = "Stove"
         assert (customer.name == "Stove")
 
     def test_customer_name_is_str(self):
-        '''customer name is a string'''
+        ''' Tests that a customer's name is a string. '''
         customer = Customer('Steve')
         assert (isinstance(customer.name, str))
 
@@ -27,7 +27,7 @@ class TestCustomer:
             customer.name = 1
 
     def test_customer_name_length(self):
-        '''customer name is between 1 and 15 characters'''
+        ''' Tests that a customer's name is between 1 and 15 characters. '''
         customer = Customer('Steve')
         assert (len(customer.name) == 5)
 
@@ -38,7 +38,7 @@ class TestCustomer:
             customer.name = ""
 
     def test_has_many_orders(self):
-        '''customer has many orders'''
+        ''' Tests that a customer can have many orders. '''
         pizza = Pizza("Cheese")
         customer_1 = Customer('Steve')
         customer_2 = Customer('Dima')
@@ -46,23 +46,23 @@ class TestCustomer:
         order_2 = Order(customer_1, pizza, 5)
         order_3 = Order(customer_2, pizza, 5)
 
-        assert (len(customer_1.orders()) == 2)
-        assert (not order_3 in customer_1.orders())
-        assert (order_1 in customer_1.orders())
-        assert (order_2 in customer_1.orders())
+        assert (len(customer_1.access_current_orders()) == 2)
+        assert (not order_3 in customer_1.access_current_orders())
+        assert (order_1 in customer_1.access_current_orders())
+        assert (order_2 in customer_1.access_current_orders())
 
     def test_orders_of_type_order(self):
-        '''customer orders are of type Order'''
+        ''' Tests that a customer's orders are each of type `Order`. '''
         pizza = Pizza("Cheese")
         customer = Customer('Steve')
         order_1 = Order(customer, pizza, 2)
         order_2 = Order(customer, pizza, 5)
 
-        assert (isinstance(customer.orders()[0], Order))
-        assert (isinstance(customer.orders()[1], Order))
+        assert (isinstance(customer.access_current_orders()[0], Order))
+        assert (isinstance(customer.access_current_orders()[1], Order))
 
     def test_has_many_pizzas(self):
-        '''customer has many pizzas.'''
+        ''' Tests that a customer can have many pizzas. '''
         pizza_1 = Pizza("Cheese")
         pizza_2 = Pizza("Mushroom")
 
@@ -70,11 +70,11 @@ class TestCustomer:
         order_1 = Order(customer, pizza_1, 2)
         order_2 = Order(customer, pizza_2, 5)
 
-        assert (pizza_1 in customer.pizzas())
-        assert (pizza_2 in customer.pizzas())
+        assert (pizza_1 in customer.access_current_pizzas())
+        assert (pizza_2 in customer.access_current_pizzas())
 
     def test_has_unique_pizzas(self):
-        '''customer has unique list of all the pizzas they have ordered.'''
+        ''' Tests that a customer has a unique list of all the pizzas that they have ordered. '''
         pizza_1 = Pizza("Cheese")
         pizza_2 = Pizza("Mushroom")
 
@@ -83,5 +83,5 @@ class TestCustomer:
         order_2 = Order(customer, pizza_1, 2)
         order_3 = Order(customer, pizza_2, 5)
 
-        assert (len(set(customer.pizzas())) == len(customer.pizzas()))
-        assert (len(customer.pizzas()) == 2)
+        assert (len(set(customer.access_current_pizzas())) == len(customer.access_current_pizzas()))
+        assert (len(customer.access_current_pizzas()) == 2)
