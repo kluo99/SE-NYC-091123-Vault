e4aac0a2c89a1f74a59d8611db29d9845f8e9694
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-10-30 08:32:27 -0400

    [UPDATE] Recreated Boilerplate File for Pizza

--- P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/classes/Pizza.py
+++ P3-Intro-to-Python/mock-code-challenges/pizzeria/lib/classes/Pizza.py
@@ -1,17 +1,17 @@
 class Pizza:
     def __init__(self, name):
         self.name = name
-        
-    def orders(self, new_order=None):
+
+    def access_current_orders(self, new_order=None):
         from classes.order import Order
         pass
-    
-    def customers(self, new_customer=None):
+
+    def access_current_customers(self, new_customer=None):
         from classes.customer import Customer
         pass
-    
-    def num_orders(self):
+
+    def calculate_total_number_of_orders(self):
         pass
     
-    def average_price(self):
+    def calculate_average_price_across_all_orders(self):
         pass
