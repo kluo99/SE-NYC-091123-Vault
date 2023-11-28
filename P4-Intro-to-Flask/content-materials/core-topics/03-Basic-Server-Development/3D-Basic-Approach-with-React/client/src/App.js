6e8f412790232b890a3c0e36bf6d5bf4afd9a561
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-11-28 14:43:47 -0500

    Added Tutorial Comments to Fetch API Part of Full-Stack Walkthrough

--- P4-Intro-to-Flask/content-materials/core-topics/03-Basic-Server-Development/3D-Basic-Approach-with-React/client/src/App.js
+++ P4-Intro-to-Flask/content-materials/core-topics/03-Basic-Server-Development/3D-Basic-Approach-with-React/client/src/App.js
@@ -2,6 +2,37 @@ import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+// 1. Get data from backend (current date).
+//      --> This happens in my JavaScript.
+//                • 1.1. Use Fetch API to retrieve data from some endpoint.
+//                      ---> We just have to fetch from OUR OWN BACKEND ROUTES!
+//                • 1.2. Create a state (`useState`) to store our retrieved backend data.
+// 2. Render date in returned JSX.
+//      --> This happens in my JSX/HTML. 
+//                • 2.1. Load state data for datetime into relevant component or HTML container.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   const [currentDate, setCurrentDate] = useState("");
 
@@ -16,7 +47,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>The current date is {currentDate}.</p>
+        <h2>The current date is <i>{currentDate}</i>.</h2>
       </header>
     </div>
   );
